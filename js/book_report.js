90a7a1a90880f553da214c6a1edde6beef2ac017
Benjamin Thurston <benjaminthurston@Benjamins-MacBook-Pro.local>
2019-06-15 08:22:58 -0400

    adding book report

--- js/book_report.js
+++ js/book_report.js
@@ -2,33 +2,48 @@ var message = '';
 var book;
 var search;
 
+// get title form user
+search = prompt('Search the book you own: type title [bible] (or type "quit" to end)');
+if (search.toLowerCase() === 'quit'){
+  // do nothing
+} else {
+ checkBooksForMatch();
+}
+
+
+
+
+
+// HELPER FUNCTIONS 
+function checkBooksForMatch(){
+  
+
+  for (var i = 0; i < books.length; i += 1) {
+    book = books[i];
+    // compare title to input search text
+    if (book.title === search){
+      console.log('match found')
+      // if match create html > message
+      message = getBookReport(book);
+      // print message to page
+      // print(message);
+    }
+  }
+}
+
 function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 }
 function getBookReport(book){
-  var report = '<h2>Title: ' + books.title + '</h2>';
-  report += '<p>Author: ' + books.author + '</p>';
-  report += '<p>Year: ' + books.year + '</p>';
-  report += '<p>Synopsis: ' + books.synopsis+ '</p>';
+  //console.log('getbook called with ', book)
+
+  var report = '<h2>Title: ' + book.title + '</h2>';
+  report += '<p>Author: ' + book.author + '</p>';
+  report += '<p>Year: ' + book.year + '</p>';
+  report += '<p>Synopsis: ' + book.synopsis + '</p>';
   return report;
 }
-while (true) {
-  search = prompt('Search the book you own: type title [bible] (or type "quit" to end)');
-  if (search === null || search.toLowerCase() === 'quit'){
-  break;
-  }
-  for (var i = 0; i < books.length; i += 1) {
-    book = books[i];
-    if (book.title === search){
-        message = getBookReport(book);
-        print(message);
-      }
-      
-
-      
-    }
-  }
 
 
 
--- js/booklist.js
+++ js/booklist.js
@@ -3,30 +3,30 @@ var books = [
    title: 'Along Came a Spider',
    author: 'James Patterson',
    year: 1993,
-   synopsis: "Along Came a Spider is the first novel in James Patterson's series about forensic psychologist Alex Cross. First published in 1993, its success has led to eighteen sequels as of 2012. It was adapted into a film of the same name in 2001, starring Morgan Freeman as Cross."
+   synopsis: 'Along Came a Spider is the first novel in James Pattersons series about forensic psychologist Alex Cross. First published in 1993, its success has led to eighteen sequels as of 2012. It was adapted into a film of the same name in 2001, starring Morgan Freeman as Cross.'
   },
   {
-   title: "Harry Potter and the Sorcerers Stone",
+   title: 'Harry Potter and the Sorcerers Stone',
    author: 'J. K. Rowling',
    year: 1997,
-   synopsis: '"Harry Potter and the Philosophers Stone" is the third-best-selling single-volume book of all time (120 million approx. sales).'
+   synopsis: 'Harry Potter and the Philosophers Stone is the third-best-selling single-volume book of all time (120 million approx. sales).'
   },
   {
    title: 'The Merchant of Death',
    author: 'D. J. MacHale',
    year: 2002,
-   synopsis: "The Merchant of Death is the first book in the Pendragon series by D. J. MacHale. It follows the adventures of Bobby Pendragon as he travels to Denduron."
+   synopsis: 'The Merchant of Death is the first book in the Pendragon series by D. J. MacHale. It follows the adventures of Bobby Pendragon as he travels to Denduron.'
   },
   {
-   title: 'Along Came a Spider',
-   author: 'James Patterson',
-   year: 1993,
-   synopsis: "Along Came a Spider is the first novel in James Patterson's series about forensic psychologist Alex Cross. First published in 1993, its success has led to eighteen sequels as of 2012. It was adapted into a film of the same name in 2001, starring Morgan Freeman as Cross."
+   title: 'bible',
+   author: 'various',
+   year: 0,
+   synopsis: 'this is the bible'
   },
   {
-   title: 'Along Came a Spider',
-   author: 'James Patterson',
-   year: 1993,
-   synopsis: "Along Came a Spider is the first novel in James Patterson's series about forensic psychologist Alex Cross. First published in 1993, its success has led to eighteen sequels as of 2012. It was adapted into a film of the same name in 2001, starring Morgan Freeman as Cross."
+   title: 'Stop',
+   author: 'why',
+   year: 2019,
+   synopsis: 'I needed a book'
   },
 ];
